9653ba3b6fb810835c718e770071cc680894f4bb
Ines Radic <inesradic12314@gmail.com>
2020-12-22 17:53:32 -0800

    Changed song name to a link to lyrics and updated appending for shazam API

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ const queryURL = "https://tastedive.com/api/similar";
 // localstorage variables
 let savedArtists = []
 
-  
-$(".lyricSearchBtn").click(function(){
-    
-    
-    
+
+$(".lyricSearchBtn").click(function () {
+
+
+
     const searchedLyrics = $('.lyricSearchBar').val();
     $.ajax({
         "async": true,
@@ -26,44 +26,44 @@ $(".lyricSearchBtn").click(function(){
             "x-rapidapi-host": "shazam.p.rapidapi.com"
         }
     }).done(function (response) {
-    console.log(response);
-    // second ajax call here
-    // code here will be executed once we get response from Shazam
-    const artistName = response.tracks.hits[0].track.subtitle;
-    const modArtist = artistName.replace(" ", "-");
-    const songName = response.tracks.hits[0].track.title
-    const songLyrics = response.tracks.hits[0].track.url
-    const songArt = response.tracks.hits[0].track.images.background
-    let artistObject = {
-        artist : artistName,
-        song : songName,
-        lyrics : songLyrics,
-        poster : songArt
-    }
-    createArtistBio(artistObject);
-    console.log(artistObject);
-    handleTasteDive(modArtist);
+        console.log(response);
+        // second ajax call here
+        // code here will be executed once we get response from Shazam
+        const artistName = response.tracks.hits[0].track.subtitle;
+        const modArtist = artistName.replace(" ", "-");
+        const songName = response.tracks.hits[0].track.title
+        const songLyrics = response.tracks.hits[0].track.url
+        const songArt = response.tracks.hits[0].track.images.background
+        let artistObject = {
+            artist: artistName,
+            song: songName,
+            lyrics: songLyrics,
+            poster: songArt
+        }
+        createArtistBio(artistObject);
+        console.log(artistObject);
+        handleTasteDive(modArtist);
 
     });
 
 
     //saveSearchedArtist(searchedLyrics);
     console.log("we're in the function");
-        
+
 })
 
-function saveSearchedArtist (artist) {
+function saveSearchedArtist(artist) {
     savedArtists.push(artist);
     window.localStorage.setItem('artists', JSON.stringify(savedArtists));
 }
 
-function handleTasteDive (modArtist) {
+function handleTasteDive(modArtist) {
     $.ajax({
         type: "GET",
         url: queryURL,
         jsonp: "callback", // not sure if we need this, if commented out, this test still works
         dataType: "jsonp", // send JSON data without worry of cross-domain issues
-        
+
         // data object is for TasteDive API calls
         data: {
             type: "music", // media form specification
@@ -71,12 +71,12 @@ function handleTasteDive (modArtist) {
             k: apiKey, // API access key for TasteDive
             info: 1, // to include a return of youtube links
         },
-        }).then(function(response) {
+    }).then(function (response) {
         // set total similar artist to 3 for displaying only 3 artists, can adjust for fewer or more
         const totalSimArtists = 3;
         // recommendedArtistsDiv.empty();
         // for each similar artist, add a list item
-        for (var i = 0; i < totalSimArtists; i++){
+        for (var i = 0; i < totalSimArtists; i++) {
             // youtube link with response ID to form html link
             let youtubeLink = "https://www.youtube.com/watch?v=" + response.Similar.Results[i].yID
             // name of similar artist
@@ -92,10 +92,10 @@ function handleTasteDive (modArtist) {
             // console.log("this is taste dive result: ", tasteDiveResults)
             console.log("td response: ", youtubeLink)
             console.log("tastdive object: ", response)
-            
+
         }
 
-        });
+    });
 }
 
 let testArtistObject = {
@@ -103,18 +103,32 @@ let testArtistObject = {
     poster: './assets/images/kanyeTestImage.jpeg',
     song: 'Power',
     lyrics: 'https://www.shazam.com/track/52699656/power'
-    
+
 }
 
-function createArtistBio (artistObject) {
+function createArtistBio(artistObject) {
     artistInfoDiv.empty();
     lyricsDiv.empty();
-    let artist = $('<div>').text(artistObject.artist);
-    let poster = $('<img>').attr('src', artistObject.poster);
-    let song = $('<div>').text(artistObject.song);
-    let lyrics = $('<a>').attr('href', artistObject.lyrics).text('Click for lyrics');
-    artistInfoDiv.append(artist).append(poster);
-    lyricsDiv.append(song).append(lyrics);
+    // Links title of song with url to lyrics.
+    let lyricsUrl = 'https://www.shazam.com/track/46697155/' + artistObject.song;
+    const songLink = artistObject.song;
+
+    // Displays artist name and poster.
+    $('<div>', {
+        id: 'artist',
+        text: artistObject.artist
+    }).append($('<img>', {
+        id: 'poster',
+        src: artistObject.poster
+    })).appendTo(artistInfoDiv);
+
+    // Displays song name as a link to the lyrics.
+    $('<a>', {
+        id: 'link-lyrics',
+        href: lyricsUrl,
+        target: '_blank',
+        text: songLink,
+    }).appendTo(lyricsDiv);
 }
 
 //createArtistBio(testArtistObject);
